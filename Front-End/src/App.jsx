c06d5253eb33c9024b4d173fed056aa89945e4c4
Bharath <95229845+BHARATH2503@users.noreply.github.com>
2023-05-07 00:45:33 +0530

    changes in color,image

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -4,35 +4,6 @@ import Expenses from "./component/expenses/expenses";
 import { NewExpense } from './component/new_expense/NewExpense';
 import { getAllExpense,addExpense } from './api';
 
-
-// const DUMMY_DATA = [
-//   {
-//     id: 'e1',
-//     title: 'Groceries',
-//     amount: 94.12,
-//     date: new Date(2021, 7, 14),
-//   },
-//   {
-//     id: 'e2', 
-//     title: 'New Mobile',
-//     amount: 799.49,
-//     date: new Date(2020, 2, 12)
-//   },
-//   {
-//     id: 'e3',
-//     title: 'Car Repair',
-//     amount: 294.67,
-//     date: new Date(2021, 2, 28),
-//   },
-//   {
-//     id: 'e4',
-//     title: 'New work desk',
-//     amount: 450,
-//     date: new Date(2021, 5, 12),
-//   },
-// ];
-
-
 function App() {
 
   const [expenses, setExpenses] = useState([]);
@@ -54,6 +25,7 @@ function App() {
 console.log(expenses)
   return (
     <div className="App">
+      <h1 className='title'>Expense Tracker</h1>
       <NewExpense submit={onAddData}/>
       <Expenses expenses={expenses}/>
     </div>
