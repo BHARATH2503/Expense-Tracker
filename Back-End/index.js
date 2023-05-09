0e854f35c608e21ee7bfff00696adf64a696d4d0
Bharath <95229845+BHARATH2503@users.noreply.github.com>
2023-05-09 14:23:53 +0530

    link set up

--- Back-End/index.js
+++ Back-End/index.js
@@ -10,15 +10,15 @@ const app = new express()
 app.use(bodyParser.json());
 app.use(cors(({origin:'*'})));
 
-app.get('   h',(req,res) =>{
+app.get('/api/v2/health',(req,res) =>{
     res.status(200).send("Health Check");
 })
 
-app.get('https://expense-tracker-9qko.onrender.com/api/v2/expenses',getAllExpenses);
-app.get('https://expense-tracker-9qko.onrender.com/api/v2/expenses/:id',getSomeExpense);
-app.post('https://expense-tracker-9qko.onrender.com/api/v2/expenses/create',createExpense);
-app.delete('https://expense-tracker-9qko.onrender.com/api/v2/expenses/:id',deleteExpense);
-app.put('https://expense-tracker-9qko.onrender.com/api/v2/expenses/update/:id',updateExpense)
+app.get('/api/v2/expenses',getAllExpenses);
+app.get('/api/v2/expenses/:id',getSomeExpense);
+app.post('/api/v2/expenses/create',createExpense);
+app.delete('/api/v2/expenses/:id',deleteExpense);
+app.put('/api/v2/expenses/update/:id',updateExpense)
 
 app.listen(3000, ()=>{
     console.log("Server running")
--- Front-End/src/api.js
+++ Front-End/src/api.js
@@ -1,11 +1,11 @@
 export const getAllExpense = (setExpenses) => {
-    fetch('http://localhost:3000/api/v2/expenses')
+    fetch('https://expense-tracker-9qko.onrender.com/api/v2/expenses')
         .then((response) => response.json())
             .then((expense_data) => setExpenses(expense_data.data));
 }
 
 export const addExpense = (postData = {}, callback) => {
-    fetch('http://localhost:3000/api/v2/expenses/create',
+    fetch('https://expense-tracker-9qko.onrender.com/api/v2/expenses/create',
     {
         method:"POST",
         headers:{
